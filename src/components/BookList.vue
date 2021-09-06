6074ebbe4c25338242da50d35744495fc9f61c80
ribex <20078694+ribex@users.noreply.github.com>
2021-09-06 12:59:11 -0400

    Connect BookItem as a child component of BookList

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,12 +2,13 @@
   <div>
     <h1>{{title}} </h1>
     <ul>
-      <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+      <book-item v-for="book in books" :book=book>{{book.title}}: {{book.author}}</book-item>
     </ul>
   </div>
 </template>
 
 <script>
+import BookItem from './BookItem';
 export default {
   name: 'BookList',
   data() {
@@ -20,6 +21,7 @@ export default {
       ]
     };
   },
+  components: {BookItem}
 };
 
 </script>
